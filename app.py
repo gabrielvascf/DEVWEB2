bece307c737c2e42293879b7362a7b9e564964ae
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2023-10-20 08:02:03 -0300

    Add templates and app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from jinja2.exceptions import TemplateNotFound
 
 app = Flask("__name__")
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/<filename>")
+def templates(filename):
+    try:
+        return render_template(f"{filename}.html")
+    except TemplateNotFound:
+        return "Página não encontrada", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
